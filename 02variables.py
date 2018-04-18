ebe65d6bfb89b813588a0bd7b673c4dcf8cb242e
Guillermo Spindola <guillermo.spindola@sm4rt.com>
2018-04-18 11:21:10 -0500

    Terminando de trabajar con el archivo de variables

--- 02variables.py
+++ 02variables.py
@@ -110,4 +110,84 @@ print(int(kobe) + int(dirk))
 flotante = 23.5
 print(int(flotante))
 
+### Operaciones numericas ###
+
+"""Se pueden utilizar los simbolos basicos de aritmetica para realizar operaciones"""
+
+#suma
+print("La suma de 2+2 es igual a:", 2+2)
+#resta
+print("La resta de 3-9 es igual a:", 3-9)
+#multiplicacion
+print("El resultado de multiplicar 5*4 es:", 5*4)
+#division
+print("La division de 4/3 da como resultado:", 4/3)
+
+
+"""Además de las operaciones básicas se pueden realizar algunas otras operaciones adicionales"""
+
+a=7
+b=2
+
+#multiplo inferior: realiza la division eliminando el resto
+print(f"{a}//{b}=", a//b)
+#modulo: realizar una division y quedarse solo con el resto
+print(f"{a}%{b}=", a%b)
+#negacion: cambiar de sentido un valor
+print(f"La negacion de {a} es:", -a)
+#potencias: elevar un numero al cuadrado, cubo, etcetera
+print(f"El numero {a} elevado a la potencia de {b} da como resultado:", a**b)
+
+### BOOLEANAS ###
+
+"""Las variables boleanas son aquellas que pueden tener un valor verdadero o falso"""
+
+verdadero = True
+falso = False
+
+print(type(verdadero))
+print(type(falso))
+
+"Como tipo primitivo especial existe None, que es una variable nula"
+
+nulo = None
+print(type(nulo))
+
+"Cualquier tipo de variable se puede convertir a boleano, transformandola en true"
+
+fruta = "platando"
+print(type(bool(fruta)))
+
+"0 se transforma en false"
+print(bool(0))
+
+"De igual manera None se convierte en false"
+print(bool(nulo))
+
+### Comparaciones Logicas ###
+#Se pueden realizar comparaciones entre variables, el resultado de estas siempre es una variable boleana
+
+"<: Este comparador significa menor que"
+print(f"El valor {a} es menor que {b}:", a < b)
+"<=: Este comparador nos indica si es menor o igual"
+print(f"El valor de {b} es menor o igual que {a}:", b <= a)
+">: Este comparador significa mayor que"
+print(f"El valor {a} es mayor que {b}:", a > b)
+">=: Este comparador nos indica mayor o igual"
+print(f"El valor {b} es mayor o igual que {a}:", b >= a)
+"==: Este comparador significa igual a"
+print(f"El número {a} es igual a {b}:", a == b)
+"!=: Este comparador nos indica que es diferente de"
+print(f"El valor de {b} es diferente a {a}:", b != a)
+"not: Con not podemos obtener el opuesto de un resultado logico"
+print(f"not {a} != 23", not a != 23)
+"and: Con and se pueden evaluar que varias condiciones se cumplan"
+print(f"{a} != 23 and {a} > {b}=", a != 23 and a > b)
+"or: Se utiliza para evaluar si se cumple una condición u otra"
+print(f"{b} >= {a} or {b} == 2 =", b >= a or b == 2)
+"is: Se utiliza para comparar si algo es verdadero o falso, en vez de utilizar =="
+print("Verdadero es True", verdadero is True)
+print("Falso es True", falso is True)
+
+
 
